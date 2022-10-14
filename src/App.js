210456ac6640f85d8e6d09aceb00c8c49e1e0027
Mohamed Adel <107929881+imoeadel@users.noreply.github.com>
2022-10-14 13:54:35 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
     <AnimatePresence>
       <div className="w-screen h-auto flex flex-col bg-primary">
         <Header />
-        <main className="mt-16 md:mt-18 px-16 py-4 w-full">
+        <main className="mt-14 md:mt-20 px-4 md:px-16 py-4 w-full">
           <Routes>
             <Route path="/*" element={<MainContainer />} />
             <Route path="/createItem" element={<CreateContainer />} />
